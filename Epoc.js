71f62a621402d3a6147c25d62b2e5751a83380f4
Charlie gerard <annecharlotte.gerard@gmail.com>
2015-10-14 20:14:09 +1100

    refactoring call to Sphero

--- Epoc.js
+++ Epoc.js
@@ -5,21 +5,25 @@ var sphero = require('sphero');
 var orb = sphero("/dev/rfcomm0");
 
 require('events').EventEmitter.prototype._maxListeners = 0;
-
+console.log('waiting for sphero connection')
 Epoc.connect(function(event) {
 	// console.log("Callback working with event: ", event);
 	// if(event.blink > 0){
 	// 	console.log("blink!")
 	// }
 
-	orb.connect(function(){
-		console.log("Sphero connected....")
+	function gyroControl(sphero){
 		if(event.gyroX > 0){
 			//Roll Sphero forward
-			orb.roll(0, 150);
+			sphero.roll(0, 150);
 		} else if(event.gyroX < 0){
 			//Roll Sphero backwards
-			orb.roll(180, 150);
+			sphero.roll(180, 150);
 		}
-	})
+	}
 });
+
+orb.connect(function(){
+	console.log("Sphero connected....")
+	gyroControl(orb);
+})
