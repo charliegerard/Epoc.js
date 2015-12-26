4144b17d314dc52a814defac75440123b3f7f635
Charlie gerard <annecharlotte.gerard@gmail.com>
2015-12-26 16:32:11 +1100

    refactor commands

--- Epoc.js
+++ Epoc.js
@@ -13,32 +13,44 @@ orb.connect(function(){
 
 function spheroControl(sphero){
 	Epoc.connect(function(event) {
+		var eyeMovement = '';
 
 		if(event.blink > 0){
-			console.log("blink!")
+			eyeMovement != "blink" ? eyeMovement = "blink" : break;
 		}
 
 		if(event.lookingLeft > 0){
-			console.log('LEFT')
-			sphero.roll(70, 0, 1);
+			eyeMovement != "left" ? eyeMovement = "left" : break;
 		}
 
 		if(event.lookingUp > 0){
-			console.log('UP')
+			eyeMovement != "up" ? eyeMovement = "up" : break;
 		}
 
 		if(event.lookingDown > 0){
-			console.log('DOWN')
+			eyeMovement != "down" ? eyeMovement = "down" : break;
 		}
 
 		if(event.lookingRight > 0){
-			console.log('RIGHT')
-			sphero.roll(90, 0, 1);
+			eyeMovement != "right" ? eyeMovement = "right" : break;
 		}
 
 		if(event.smile > 0){
-			console.log('SMILE')
-			sphero.roll(0, 0, 0);
+			eyeMovement != "smile" ? eyeMovement = "smile" : break;
 		}
+
+		switch(eyeMovement){
+			case 'left':
+				sphero.roll(70, 0, 1);
+				break;
+			case 'right':
+				sphero.roll(70, 90, 1);
+				break;
+			case 'smile':
+				sphero.roll(0, 0, 1);
+				break;
+		}
+
+		console.log('Direction: ', eyeMovement);
 	});
 }
